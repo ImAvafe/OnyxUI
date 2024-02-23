1c34b4f89cffefba3209e52d7e12f8572b09e5ab
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-02-23 11:43:14 -0500

    curate onyxnight theme

--- src/Utils/Themer/OnyxNight.lua
+++ src/Utils/Themer/OnyxNight.lua
@@ -1,3 +1,59 @@
+local OnyxUI = script.Parent.Parent.Parent
+
+local Colors = require(OnyxUI.Utils.Colors)
+
 return {
-	BackgroundColor = Color3.fromRGB(26, 26, 26),
+	Colors = {
+		Primary = {
+			Main = Colors.White,
+		},
+		Secondary = {
+			Main = Colors.White,
+		},
+		Accent = {
+			Main = Colors.White,
+		},
+
+		Neutral = {
+			Main = Colors.Stone["800"],
+		},
+		NeutralContent = {
+			Main = Colors.Stone["300"],
+		},
+
+		Base = {
+			Main = Colors.Stone["900"],
+		},
+		BaseContent = {
+			Main = Colors.White,
+		},
+
+		Success = {
+			Main = Colors.Green["500"],
+		},
+		Error = {
+			Main = Colors.Red["500"],
+		},
+		Warning = {
+			Main = Colors.Amber["500"],
+		},
+		Info = {
+			Main = Colors.Cyan["400"],
+		},
+	},
+	Fonts = {
+		Body = "GothamSsm",
+		Heading = "GothamSsm",
+		Monospace = "GothamSsm",
+	},
+	FontWeights = {
+		Body = Enum.FontWeight.Regular,
+		Bold = Enum.FontWeight.Bold,
+		Heading = Enum.FontWeight.Bold,
+	},
+	TextSize = 18,
+	CornerRadius = 8,
+	StrokeWidth = 2,
+	Space = 4,
+	Transitions = {},
 }
