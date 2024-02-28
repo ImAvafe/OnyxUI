9848eda5f055494faa8453ec0abf26f9a5749ca9
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-02-28 14:37:04 -0500

    Add BitCave theme

---
+++ src/Utils/Themer/BitCave.lua
@@ -0,0 +1,59 @@
+local OnyxUI = script.Parent.Parent.Parent
+
+local Colors = require(OnyxUI.Utils.Colors)
+
+return {
+	Colors = {
+		Primary = {
+			Main = Colors.White,
+		},
+		Secondary = {
+			Main = Colors.White,
+		},
+		Accent = {
+			Main = Colors.White,
+		},
+
+		Neutral = {
+			Main = Colors.Stone["700"],
+		},
+		NeutralContent = {
+			Main = Colors.Stone["300"],
+		},
+
+		Base = {
+			Main = Colors.Black,
+		},
+		BaseContent = {
+			Main = Colors.White,
+		},
+
+		Success = {
+			Main = Colors.Green["500"],
+		},
+		Error = {
+			Main = Colors.Red["500"],
+		},
+		Warning = {
+			Main = Colors.Amber["500"],
+		},
+		Info = {
+			Main = Colors.Cyan["400"],
+		},
+	},
+	Fonts = {
+		Body = "rbxassetid://12187371840",
+		Heading = "rbxassetid://12187371840",
+		Monospace = "rbxassetid://12187371840",
+	},
+	FontWeights = {
+		Body = Enum.FontWeight.Bold,
+		Bold = Enum.FontWeight.ExtraBold,
+		Heading = Enum.FontWeight.ExtraBold,
+	},
+	TextSize = 18,
+	CornerRadius = 0,
+	StrokeThickness = 2,
+	Space = 8,
+	SpringSpeed = 80,
+}
