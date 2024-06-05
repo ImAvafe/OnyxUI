0fed0509ad9884d61d9387449f47be5a7b2217c1
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-06-05 14:09:57 -0400

    Update Text component to new standards

--- src/Components/Text.lua
+++ src/Components/Text.lua
@@ -1,73 +1,67 @@
 local OnyxUI = require(script.Parent.Parent)
 local Fusion = require(OnyxUI.Packages.Fusion)
-
 local EnsureValue = require(OnyxUI.Utils.EnsureValue)
 local Themer = require(OnyxUI.Utils.Themer)
 
-local New = Fusion.New
-local Children = Fusion.Children
-local Out = Fusion.Out
 local Computed = Fusion.Computed
+local Hydrate = Fusion.Hydrate
 
-local function Text(Props: { [any]: any })
-	Props.Name = EnsureValue(Props.Name, "string", "Text")
-	Props.AutomaticSize = EnsureValue(Props.AutomaticSize, "EnumItem", Enum.AutomaticSize.XY)
-	Props.TextColor3 = EnsureValue(Props.TextColor3, "Color3", Themer.Theme.Colors.BaseContent.Main)
-	Props.TextSize = EnsureValue(Props.TextSize, "number", Themer.Theme.TextSize["1"])
-	Props.RichText = EnsureValue(Props.RichText, "boolean", true)
-	Props.FontFace = EnsureValue(
+local Base = require(script.Parent.Base)
+local PubTypes = require(script.Parent.Parent.PubTypes)
+local CombineProps = require(script.Parent.Parent.Utils.CombineProps)
+
+type Props = Base.BaseProps & {
+	Text: PubTypes.CanBeState<string>?,
+	TextColor3: PubTypes.CanBeState<Color3>?,
+	TextSize: PubTypes.CanBeState<number>?,
+	RichText: PubTypes.CanBeState<boolean>?,
+	FontFace: PubTypes.CanBeState<Font>?,
+	TextWrapped: PubTypes.CanBeState<boolean>?,
+	TextXAlignment: PubTypes.CanBeState<Enum.TextXAlignment>?,
+	TextYAlignment: PubTypes.CanBeState<Enum.TextYAlignment>?,
+	Font: PubTypes.CanBeState<Enum.Font>?,
+	TextScaled: PubTypes.CanBeState<boolean>?,
+	TextTruncate: PubTypes.CanBeState<boolean>?,
+	LineHeight: PubTypes.CanBeState<number>?,
+	LocalizedText: PubTypes.CanBeState<string>?,
+	MaxVisibleGraphemes: PubTypes.CanBeState<number>?,
+	TextTransparency: PubTypes.CanBeState<number>?,
+}
+
+local function Text(Props: Props)
+	local Name = EnsureValue(Props.Name, "string", "Text")
+	local AutomaticSize = EnsureValue(Props.AutomaticSize, "EnumItem", Enum.AutomaticSize.XY)
+	local TextColor3 = EnsureValue(Props.TextColor3, "Color3", Themer.Theme.Colors.BaseContent.Main)
+	local TextSize = EnsureValue(Props.TextSize, "number", Themer.Theme.TextSize["1"])
+	local RichText = EnsureValue(Props.RichText, "boolean", true)
+	local FontFace = EnsureValue(
 		Props.FontFace,
 		"Font",
 		Computed(function()
 			return Font.new(Themer.Theme.Font.Body:get(), Themer.Theme.FontWeight.Body:get())
 		end)
 	)
-	Props.TextWrapped = EnsureValue(Props.TextWrapped, "boolean", true)
-	Props.ClipsDescendants = EnsureValue(Props.ClipsDescendants, "boolean", true)
-	Props.TextXAlignment = EnsureValue(Props.TextXAlignment, "EnumItem", Enum.TextXAlignment.Left)
-	Props.TextYAlignment = EnsureValue(Props.TextYAlignment, "EnumItem", Enum.TextYAlignment.Top)
-	Props.BackgroundTransparency = EnsureValue(Props.BackgroundTransparency, "number", 1)
+	local TextWrapped = EnsureValue(Props.TextWrapped, "boolean", true)
+	local ClipsDescendants = EnsureValue(Props.ClipsDescendants, "boolean", false)
+	local TextXAlignment = EnsureValue(Props.TextXAlignment, "EnumItem", Enum.TextXAlignment.Left)
+	local TextYAlignment = EnsureValue(Props.TextYAlignment, "EnumItem", Enum.TextYAlignment.Top)
+	local BackgroundTransparency = EnsureValue(Props.BackgroundTransparency, "number", 1)
 
-	return New "TextLabel" {
-		Name = Props.Name,
-		Parent = Props.Parent,
-		Position = Props.Position,
-		Rotation = Props.Rotation,
-		AnchorPoint = Props.AnchorPoint,
-		Size = Props.Size,
-		AutomaticSize = Props.AutomaticSize,
-		Visible = Props.Visible,
-		ZIndex = Props.ZIndex,
-		LayoutOrder = Props.LayoutOrder,
-		ClipsDescendants = Props.ClipsDescendants,
-		Active = Props.Active,
-		Selectable = Props.Selectable,
-		Interactable = Props.Interactable,
-		BackgroundColor3 = Props.BackgroundColor3,
-		BackgroundTransparency = Props.BackgroundTransparency,
-
-		RichText = Props.RichText,
-		TextSize = Props.TextSize,
-		TextColor3 = Props.TextColor3,
-		FontFace = Props.FontFace,
-		Font = Props.Font,
-		TextScaled = Props.TextScaled,
+	return Hydrate(Base(CombineProps(Props, {
+		ClassName = "TextLabel",
+		Name = Name,
+		AutomaticSize = AutomaticSize,
+	}))) {
 		Text = Props.Text,
-		TextWrapped = Props.TextWrapped,
-		TextXAlignment = Props.TextXAlignment,
-		TextYAlignment = Props.TextYAlignment,
-		TextTruncate = Props.TextTruncate,
-		AutoLocalize = Props.AutoLocalize,
-		LineHeight = Props.LineHeight,
-		LocalizedText = Props.LocalizedText,
-		MaxVisibleGraphemes = Props.MaxVisibleGraphemes,
-		TextTransparency = Props.TextTransparency,
-
-		[Out "ContentText"] = Props.ContentText,
-		[Out "TextBounds"] = Props.TextBounds,
-		[Out "TextFits"] = Props.TextFits,
-
-		[Children] = Props[Children],
+		TextColor3 = TextColor3,
+		TextSize = TextSize,
+		RichText = RichText,
+		FontFace = FontFace,
+		TextWrapped = TextWrapped,
+		ClipsDescendants = ClipsDescendants,
+		TextXAlignment = TextXAlignment,
+		TextYAlignment = TextYAlignment,
+		BackgroundTransparency = BackgroundTransparency,
 	}
 end
 
