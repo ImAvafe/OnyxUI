048e06222edacce2e9f1df2ebe6847b70a5a3429
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-06-05 13:25:07 -0400

    Migrate AutoScaleFrame to new standards

--- src/Components/AutoScaleFrame.lua
+++ src/Components/AutoScaleFrame.lua
@@ -1,59 +1,45 @@
+local Workspace = game:GetService("Workspace")
 local OnyxUI = require(script.Parent.Parent)
 local Fusion = require(OnyxUI.Packages.Fusion)
 local EnsureValue = require(OnyxUI.Utils.EnsureValue)
-local Modifier = require(OnyxUI.Utils.Modifier)
+local PubTypes = require(script.Parent.Parent.PubTypes)
 
 local Value = Fusion.Value
 local Computed = Fusion.Computed
 local Out = Fusion.Out
 local Hydrate = Fusion.Hydrate
-local Children = Fusion.Children
 
-local Frame = require(OnyxUI.Components.Frame)
+local Base = require(script.Parent.Base)
+local Frame = require(script.Parent.Frame)
 
-return function(Props: { [any]: any })
-	Props.Name = EnsureValue(Props.Name, "string", "AutoScaleFrame")
+type Props = Base.BaseProps & {
+	BaseResolution: PubTypes.CanBeState<Vector2>,
+	MinScale: PubTypes.CanBeState<number>?,
+	MaxScale: PubTypes.CanBeState<number>?,
+	ScaleMultiplier: PubTypes.CanBeState<number>?,
+}
 
-	Props.BaseResolution = EnsureValue(Props.BaseResolution, "Vector2", Vector2.new())
-	Props.ScaleClamps = EnsureValue(Props.ScaleClamps, "table", { Min = 0.8, Max = math.huge })
-	Props.ScaleMultiplier = EnsureValue(Props.ScaleMultiplier, "number", 1)
+return function(Props: Props)
+	local Name = EnsureValue(Props.Name, "string", "AutoScaleFrame")
+	local BaseResolution = EnsureValue(Props.BaseResolution, "Vector2", Vector2.new())
+	local MinScale = EnsureValue(Props.MinScale, "number", 0.8)
+	local MaxScale = EnsureValue(Props.MaxScale, "number", math.huge)
+	local ScaleMultiplier = EnsureValue(Props.ScaleMultiplier, "number", 1)
 
 	local ViewportSize = Value(Vector2.new())
-	Hydrate(game.Workspace.CurrentCamera) {
+	Hydrate(Workspace.CurrentCamera) {
 		[Out "ViewportSize"] = ViewportSize,
 	}
 
 	return Frame {
-		Name = Props.Name,
-		Parent = Props.Parent,
-		Position = Props.Position,
-		Rotation = Props.Rotation,
-		AnchorPoint = Props.AnchorPoint,
-		Size = Props.Size,
-		AutomaticSize = Props.AutomaticSize,
-		Visible = Props.Visible,
-		ZIndex = Props.ZIndex,
-		LayoutOrder = Props.LayoutOrder,
-		ClipsDescendants = Props.ClipsDescendants,
-		Active = Props.Active,
-		Selectable = Props.Selectable,
-		Interactable = Props.Interactable,
-		BackgroundColor3 = Props.BackgroundColor3,
-		BackgroundTransparency = Props.BackgroundTransparency,
-
-		[Children] = {
-			Modifier.Scale {
-				Scale = Computed(function()
-					local Ratio = Props.ScaleMultiplier:get()
-						/ math.max(
-							(Props.BaseResolution:get().X / ViewportSize:get().X),
-							(Props.BaseResolution:get().Y / ViewportSize:get().Y)
-						)
-					return math.clamp(Ratio, Props.ScaleClamps:get().Min, Props.ScaleClamps:get().Max)
-				end),
-			},
-
-			Props[Children],
-		},
+		Name = Name,
+		Scale = Computed(function()
+			local Ratio = ScaleMultiplier:get()
+				/ math.max(
+					(BaseResolution:get().X / ViewportSize:get().X),
+					(BaseResolution:get().Y / ViewportSize:get().Y)
+				)
+			return math.clamp(Ratio, MinScale:get(), MaxScale:get())
+		end),
 	}
 end
