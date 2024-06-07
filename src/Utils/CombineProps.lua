64dcd11e992f77e25dfae97f3136d34224637c0a
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-06-07 12:11:17 -0400

    Allow excluding prop keys from CombineProps

--- src/Utils/CombineProps.lua
+++ src/Utils/CombineProps.lua
@@ -1,5 +1,9 @@
-local function CombineProps(Source: { [string]: any }, Target: { [string]: any })
+local function CombineProps(Source: { any: any }, Target: { any: any }, ExcludedKeys: { any }?)
 	for Key, Value in pairs(Source) do
+		if (ExcludedKeys ~= nil) and (table.find(ExcludedKeys, Key) ~= nil) then
+			continue
+		end
+
 		Target[Key] = Value
 	end
 
